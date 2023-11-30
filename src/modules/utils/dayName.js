12b74002d7cabc5002009a689fb0a7e33c3a9076
sinned8 <dennisdust2@gmail.com>
2023-11-30 12:25:29 -0500

    Fixed gitignore and added a kelvin to cel conversion a file for the individual day names stored in an array which can be indexed and a validation function

---
+++ src/modules/utils/dayName.js
@@ -0,0 +1,16 @@
+const index =  new Date().getDay()
+
+export default function getDayName(i) {
+    let dayList = [
+        "sunday",
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+    ];
+    return dayList[i]
+}
+
+export const today = getDayName(index)
