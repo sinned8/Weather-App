48ffe0a5d7a82504fc45be925f618dea1241628a
sinned8 <dennisdust2@gmail.com>
2023-12-06 15:19:24 -0500

    Added function in index file that shows initial API integration is succesful, need to add working bttns and a seperate file for geolocation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css"
+import searchImg from './imgs/magnify.svg'
+import locationIcon from './imgs/map-marker-outline.svg'
+import { arrangeData, showError } from "./modules/view.js"
+import extractData from "./modules/weatherData.js"
+import kToC from './modules/utils/kelvintoCelsius.js'
+
+function displayWeather(city, converter){
+    extractData(city).then((dataObj) => {
+        arrangeData(dataObj, converter);
+        document.querySelector('.main').classList.remove('hide')
+        document.querySelector('.error').classList.add('hide')
+    })
+    .catch((err) => showError(err.message))
+}
+
+window.onload = () => displayWeather('miami',kToC)
+
+export default displayWeather
